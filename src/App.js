49850fc5137dbba96721e6d24753e5b028e432d2
Ovais Butt <ovais.butt@surveyauto.com>
2020-03-30 20:20:45 +0500

    Phase 2

--- src/App.js
+++ src/App.js
@@ -1,87 +1,24 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
+import Counter from "./app/components/Counter";
 
 export default class App extends Component {
-  state = {
-    remaining: {
-      days: 0,
-      hours: 0,
-      minutes: 0,
-      seconds: 0
-    },
-    isExpired: false
-  };
-  // used to set and clear interval
-  timer;
-  // used to calculate the distance between "current date and time" and the "target date and time"
-  distance;
-
-  componentDidMount() {
-    this.setDate();
-    this.counter();
-  }
-
-  setDate = () => {
-    const { targetDate, targetTime } = this.props,
-      // Get todays date and time
-      now = new Date().getTime(),
-      // Set the date we're counting down to
-      countDownDate = new Date(targetDate + " " + targetTime).getTime();
-
-    // Find the distance between now and the count down date
-    this.distance = countDownDate - now;
-
-    // target date and time is less than current date and time
-    if (this.distance < 0) {
-      clearInterval(this.timer);
-      this.setState({ isExpired: true });
-    } else {
-      this.setState({
-        remaining: {
-          days: Math.floor(this.distance / (1000 * 60 * 60 * 24)),
-          hours: Math.floor(
-            (this.distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-          ),
-          minutes: Math.floor((this.distance % (1000 * 60 * 60)) / (1000 * 60)),
-          seconds: Math.floor((this.distance % (1000 * 60)) / 1000)
-        },
-        isExpired: false
-      });
-    }
-  };
-
-  counter = () => {
-    this.timer = setInterval(() => {
-      this.setDate();
-    }, 1000);
-  };
-
   render() {
-    const { remaining, isExpired } = this.state,
-      { targetDate, targetTime } = this.props;
-
     return (
-      <React.Fragment>
-        <h6>Waleed & Tooba</h6>
-        {!isExpired && targetDate && targetTime ? (
-          <div className="counter">
-            {Object.entries(remaining).map((el, i) => (
-              <div key={i} className="entry">
-                <div key={el[1]} className="entry-value">
-                  <span className="count top curr flipTop">{el[1] + 1}</span>
-                  <span className="count top next">{el[1]}</span>
-                  <span className="count bottom next flipBottom">{el[1]}</span>
-                  <span className="count bottom curr">{el[1] + 1}</span>
-                </div>
-                <div className="entry-title">{el[0].toUpperCase()}</div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <p className="alert-danger">Expired</p>
-        )}
-      </React.Fragment>
+      <div className="d-flex justify-content-center align-self-center align-items-center  flex-column">
+        <div  className="d-flex justify-content-center align-self-center align-items-center  flex-column">
+          <h1 style={{ color: "white", fontSize: 70 }}>Waleed &amp; Tooba</h1>
+          <h1 style={{ color: "white" }}>Save the date: 7-november-2020</h1>
+          <h1 style={{ color: "white" }}>Venue: Somewhere on earth</h1>
+        </div>
+        <div className="mt-3">
+          <Counter targetDate="Nov 07, 2020" targetTime="18:00:00" />
+        </div>
+        <div className="mt-3">
+          <img src={require("./app/assets/couple1")} alt="couple" />
+        </div>
+      </div>
     );
   }
 }
