73bbbb85d78af64bcae256b1eba93d2fff12f020
Ovais Butt <ovais.butt@surveyauto.com>
2020-03-30 20:39:32 +0500

    Fixed scroll in mobile view

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Counter from "./app/components/Counter";
 export default class App extends Component {
   render() {
     return (
-      <div className="d-flex justify-content-center align-self-center align-items-center  flex-column">
-        <div  className="d-flex justify-content-center align-self-center align-items-center  flex-column">
-          <h1 style={{ color: "white", fontSize: 70 }}>Waleed &amp; Tooba</h1>
-          <h1 style={{ color: "white" }}>Save the date: 7th-november-2020</h1>
+      <div className="d-flex justify-content-center align-self-center align-items-center flex-column col-sm-12">
+        <div className="d-flex justify-content-center align-self-center align-items-center flex-column">
+          <h1 style={{ color: "white", fontSize: 70, marginTop: 20 }} >Waleed &amp; Tooba</h1>
+          <h1 style={{ color: "white" }}>Save the date: 7th-November-2020</h1>
           <h1 style={{ color: "white" }}>Venue: Somewhere on earth</h1>
         </div>
-        <div className="mt-3">
+        <div className="mt-3 col-sm-3">
           <Counter targetDate="Nov 07, 2020" targetTime="18:00:00" />
         </div>
         <div className="mt-3">
